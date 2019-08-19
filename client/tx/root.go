3eb8c044a3286c2ae909423e0aff403960c9c6d8
Yun <yun@terra.money>
2019-08-19 14:33:16 +0900

    disable /txs query

--- client/tx/root.go
+++ client/tx/root.go
@@ -11,7 +11,7 @@ import (
 // register REST routes
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router, cdc *codec.Codec) {
 	r.HandleFunc("/txs/{hash}", sdktx.QueryTxRequestHandlerFn(cdc, cliCtx)).Methods("GET")
-	r.HandleFunc("/txs", QueryTxsByTagsRequestHandlerFn(cliCtx, cdc)).Methods("GET")
+	// r.HandleFunc("/txs", QueryTxsByTagsRequestHandlerFn(cliCtx, cdc)).Methods("GET")
 	r.HandleFunc("/txs", sdktx.BroadcastTxRequest(cliCtx, cdc)).Methods("POST")
 	r.HandleFunc("/txs/encode", sdktx.EncodeTxRequestHandlerFn(cdc, cliCtx)).Methods("POST")
 }
